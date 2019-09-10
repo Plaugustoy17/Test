e120e42ca8f06fa284d0f323f092ddcc39f60027
unknown <Aermvachirakul@gmail.com>
2019-09-10 17:16:27 +0700

    first-commit

---
+++ scrape.py
@@ -0,0 +1,10 @@
+import requests
+import urllib.request
+from bs4 import BeautifulSoup
+import re
+import datetime
+from time import time, mktime
+website_name = 'https://nameberry.com/popular_names/Nameberry'
+r_name = urllib.request.urlopen(website_name).read()
+soup_name = BeautifulSoup(r_name ,'html.parser')
+print(soup_name)
