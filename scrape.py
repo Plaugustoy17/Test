cd59c61733e1b85b6e5885294ec01598c8c255eb
unknown <Aermvachirakul@gmail.com>
2019-09-10 17:20:51 +0700

    test-v.2

--- scrape.py
+++ scrape.py
@@ -8,3 +8,4 @@ website_name = 'https://nameberry.com/popular_names/Nameberry'
 r_name = urllib.request.urlopen(website_name).read()
 soup_name = BeautifulSoup(r_name ,'html.parser')
 print(soup_name)
+# test edit
